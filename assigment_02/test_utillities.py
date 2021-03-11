d05942c6bf8e863b7302f96e1700f9ba83b66e56
tdxa <anndoman3@gmail.com>
2021-03-11 21:46:03 +0100

    Edit comments

--- assigment_02/test_utillities.py
+++ assigment_02/test_utillities.py
@@ -5,6 +5,6 @@ from utilities import filehash
 
 class TestUtillities(unittest.TestCase):
     def test_filehash(self):
-        path = 'C:\\Users\\Ania\\Downloads\\ubuntu-20.04.2.0-desktop-amd64.iso'
+        path = '.\\ubuntu-20.04.2.0-desktop-amd64.iso'
         result = filehash(path)
         self.assertEqual(result, "93bdab204067321ff131f560879db46bee3b994bf24836bb78538640f689e58f")
